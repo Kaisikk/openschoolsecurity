4a8ed70a055f43eb73b7badc08e668414aba8b9e
Kaisikk <Kosikov.Ilya@mail.ru>
2024-09-30 20:08:53 +0400

    Создание базовых настроек

---
+++ src/main/java/com/kaisik/openschoolsecurity/OpenschoolsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.kaisik.openschoolsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenschoolsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenschoolsecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaisik/openschoolsecurity/OpenschoolsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisik.openschoolsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenschoolsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
